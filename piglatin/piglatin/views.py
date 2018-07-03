1750d0aad0992756e7cdd4be1e617107932743c7
prateek tater <prateektater@gmail.com>
2018-07-03 18:07:13 -0400

    translation page

--- piglatin/piglatin/views.py
+++ piglatin/piglatin/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 def home(request):
 	return render(request, 'home.html')
 
+def translate(request):
+	original = request.GET['original']
+
+	translation = ''
+	for word in original.split():
+		if word[0] in ['a', 'e', 'i', 'o', 'u']:
+			translation += word
+			translation += 'yay'
+
+		else:
+			translation += word[1:]
+			translation += word[0]
+			translation += ay
+
+			translation += 'consonant'
+	return render(request, 'translate.html')
